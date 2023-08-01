bfd0e399f495b5c4c6f51d4160351aba2fbd9a62
Stephanie Ingrid <stephanieingrid5@gmail.com>
2023-08-01 20:41:38 -0300

    first commit

---
+++ src/main/java/io/github/stephanieingrid/VendasApplication.java
@@ -0,0 +1,16 @@
+package io.github.stephanieingrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class VendasApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(VendasApplication.class, args);
+
+
+    }
+}
