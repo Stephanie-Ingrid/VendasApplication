9927f15ca0f033006b12623e4ca0bc3eb527821e
Stephanie Ingrid <stephanieingrid5@gmail.com>
2023-08-04 00:03:11 -0300

    Adicionando application.properties

--- src/main/java/io/github/stephanieingrid/MyConfig.java
+++ src/main/java/io/github/stephanieingrid/MyConfig.java
@@ -6,9 +6,4 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MyConfig {
 
-
-    @Bean(name = "applicationName")
-    public String applicationName(){
-        return "Sistema de Vendas";
-    }
 }
--- src/main/java/io/github/stephanieingrid/VendasApplication.java
+++ src/main/java/io/github/stephanieingrid/VendasApplication.java
@@ -1,7 +1,7 @@
 package io.github.stephanieingrid;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VendasApplication {
 
-        @Autowired
-        @Qualifier("applicationName")
+        @Value("${application.name}")
+
         private String applicationName;
 
         @GetMapping ("/page")
