ef57da9904fb8493a8215507088e21d5175d1f7c
Stephanie Ingrid <stephanieingrid5@gmail.com>
2023-08-04 00:40:08 -0300

    Criando Annotations customizadas de configuração

---
+++ src/main/java/io/github/stephanieingrid/Development.java
@@ -0,0 +1,17 @@
+package io.github.stephanieingrid;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Configuration
+@Profile("development")
+public @interface Development {
+}
--- src/main/java/io/github/stephanieingrid/MyConfig.java
+++ src/main/java/io/github/stephanieingrid/MyConfig.java
@@ -1,9 +1,19 @@
 package io.github.stephanieingrid;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
+@Development
 public class MyConfig {
+
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("rodando a configuração de desenvolvimento");
+        };
+    }
 
 }
