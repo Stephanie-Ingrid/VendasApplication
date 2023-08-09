a0b42e462a637f3054508d8a0698bccabbd925d4
Stephanie Ingrid <stephanieingrid5@gmail.com>
2023-08-08 23:08:20 -0300

    Atualização e criação de scripts do banco de dados

--- src/main/java/io/github/stephanieingrid/VendasApplication.java
+++ src/main/java/io/github/stephanieingrid/VendasApplication.java
@@ -1,28 +1,15 @@
 package io.github.stephanieingrid;
 
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class VendasApplication {
-
-        @Value("${application.name}")
-
-        private String applicationName;
-
-        @GetMapping ("/page")
-            public String Page(){
-            return applicationName;
-        }
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
 
     }
 
 
-    }
+}
