4fce42ed9992d1d799a087cd0c3bfbebb308696c
Stephanie Ingrid <stephanieingrid5@gmail.com>
2023-08-14 19:35:40 -0300

    Trabalhando com @query

--- src/main/java/io/github/stephanieingrid/VendasApplication.java
+++ src/main/java/io/github/stephanieingrid/VendasApplication.java
@@ -23,33 +23,9 @@ public class VendasApplication {
             clientes.save(new Cliente("Stephanie"));
             clientes.save(new Cliente("Cliente 2"));
 
-            List<Cliente> todosClientes = clientes.findAll();
-            todosClientes.forEach(System.out::println);
+            boolean existe = clientes.existsByNome("Stephanie");
+            System.out.println("existe um cliente com o nome stephanie? " + existe);
 
-            System.out.println("Atualizando clientes");
-            todosClientes.forEach(c ->{
-                c.setNome(c.getNome() + " atualizado.");
-                clientes.save(c);
-            });
-
-            todosClientes = clientes.findAll();
-            todosClientes.forEach(System.out::println);
-
-            System.out.println("Bucando cliente");
-            clientes.findByNomeLike("Cli").forEach(System.out::println);
-
-            System.out.println("Deletando cliente");
-            clientes.findAll().forEach(c ->{
-                clientes.delete(c);
-            });
-
-            todosClientes = clientes.findAll();
-            if (todosClientes.isEmpty()){
-                System.out.println("Nenhum cliente encontrado. ");
-
-            }else {
-                todosClientes.forEach(System.out::println);
-            }
 
         };
     }
--- src/main/java/io/github/stephanieingrid/domain/repository/Clientes.java
+++ src/main/java/io/github/stephanieingrid/domain/repository/Clientes.java
@@ -3,11 +3,23 @@ package io.github.stephanieingrid.domain.repository;
 
 import io.github.stephanieingrid.domain.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface Clientes extends JpaRepository<Cliente, Integer> {
 
-   List<Cliente> findByNomeLike(String nome);
+
+   @Query(value = " select * from cliente c where c.nome like '%:nome%' ", nativeQuery = true)
+   List<Cliente> EncontrarPorNome(@Param("nome") String nome);
+
+   @Query("delete from Cliente c where c.nome =:nome")
+   @Modifying
+   void deleteByNome(String nome);
+
+   boolean existsByNome(String nome);
+
 }
 
