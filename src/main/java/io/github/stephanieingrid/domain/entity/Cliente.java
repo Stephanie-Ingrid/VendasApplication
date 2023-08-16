fe6416db59d0f34ed668b0e08c035e117cefe1bc
Stephanie Ingrid <stephanieingrid5@gmail.com>
2023-08-16 12:36:46 -0300

    Mapeamento de entidades e relacionamento

--- src/main/java/io/github/stephanieingrid/domain/entity/Cliente.java
+++ src/main/java/io/github/stephanieingrid/domain/entity/Cliente.java
@@ -1,6 +1,7 @@
 package io.github.stephanieingrid.domain.entity;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Table(name = "cliente")
@@ -15,7 +16,18 @@ public class Cliente {
     @Column(name = "nome", length = 100)
     private String nome;
 
+    @OneToMany(mappedBy = "cliente")
+    private Set<Pedido> pedidos;
+
     public Cliente() {
+    }
+
+    public Set<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(Set<Pedido> pedidos) {
+        this.pedidos = pedidos;
     }
 
     public Cliente(Integer id, String nome) {
--- src/main/java/io/github/stephanieingrid/domain/entity/Pedido.java
+++ src/main/java/io/github/stephanieingrid/domain/entity/Pedido.java
@@ -1,14 +1,28 @@
 package io.github.stephanieingrid.domain.entity;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 public class Pedido {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
     private Cliente cliente;
+
+    @Column(name = "data_pedido")
     private LocalDate dataPedido;
+
+    @Column(name = "total", length = 20, precision = 2)
     private BigDecimal Total;
+
+    @OneToMany(mappedBy = "pedido")
+    private List<ItemPedido>itens;
 
     public Integer getId() {
         return id;
