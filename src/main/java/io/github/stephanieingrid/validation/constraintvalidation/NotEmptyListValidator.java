411a4b387a76f1ffba9ec6fe9e2d90e32cb77148
Stephanie Ingrid <stephanieingrid5@gmail.com>
2023-09-09 14:55:53 -0300

    Criando anotação e classe de validação dos campos

---
+++ src/main/java/io/github/stephanieingrid/validation/constraintvalidation/NotEmptyListValidator.java
@@ -0,0 +1,20 @@
+package io.github.stephanieingrid.validation.constraintvalidation;
+
+import io.github.stephanieingrid.validation.NotEmptyList;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.List;
+
+public class NotEmptyListValidator implements ConstraintValidator <NotEmptyList, List>{
+
+    @Override
+    public boolean isValid(List list, ConstraintValidatorContext constraintValidatorContext) {
+        return list != null && !list.isEmpty();
+    }
+
+    @Override
+    public void initialize(NotEmptyList constraintAnnotation) {
+    }
+
+}
