077436de18ab92553381d0b0563a21048ac54e41
Stephanie Ingrid <stephanieingrid5@gmail.com>
2023-09-18 17:39:53 -0300

    modified

--- src/main/java/io/github/stephanieingrid/config/SecurityConfig.java
+++ src/main/java/io/github/stephanieingrid/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package io.github.stephanieingrid.config;
 
+import io.github.stephanieingrid.service.impl.UsuarioServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,19 +14,38 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UsuarioServiceImpl usuarioService;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
         }
-
-
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+    protected void configure( AuthenticationManagerBuilder auth ) throws Exception {
+            auth
+                .userDetailsService(usuarioService)
+                .passwordEncoder(passwordEncoder());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                    .antMatchers("/api/clientes/**")
+                        .hasAnyRole("USER", "ADMIN")
+
+                    .antMatchers("/api/pedidos/**")
+                        .hasAnyRole("USER", "ADMIN")
+
+                    .antMatchers("/api/produtos/**")
+                        .hasRole("ADMIN")
+
+                    .antMatchers(HttpMethod.POST, "/api/usuarios/**")
+                        .permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                    .httpBasic();
     }
 }
