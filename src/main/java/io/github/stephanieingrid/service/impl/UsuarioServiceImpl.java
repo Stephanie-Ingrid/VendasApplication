079e9570a392fac98968879c4557e2d2d1659caf
Stephanie Ingrid <stephanieingrid5@gmail.com>
2023-09-18 17:38:44 -0300

    ServiceImpl Usuario

---
+++ src/main/java/io/github/stephanieingrid/service/impl/UsuarioServiceImpl.java
@@ -0,0 +1,44 @@
+package io.github.stephanieingrid.service.impl;
+
+import io.github.stephanieingrid.domain.entity.Usuario;
+import io.github.stephanieingrid.domain.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UsuarioServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario){
+        return usuarioRepository.save(usuario);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository
+                .findByLogin(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario não encontrado. "));
+
+        String[] roles = usuario.isAdmin() ?
+                new String[]{"ADMIN", "USER"} : new String[]{"USER"};
+
+        return User
+                .builder()
+                .username(usuario.getLogin())
+                .password(usuario.getSenha())
+                .roles("ADMIN","USER")
+                .build();
+    }
+}
