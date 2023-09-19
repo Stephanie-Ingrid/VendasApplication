5a1bd3d951dbf017f9b106cd09340627daa70401
Stephanie Ingrid <stephanieingrid5@gmail.com>
2023-09-19 20:35:03 -0300

    JWT Service configurando token

---
+++ src/main/java/io/github/stephanieingrid/JwtService.java
@@ -0,0 +1,48 @@
+package io.github.stephanieingrid;
+
+import io.github.stephanieingrid.domain.entity.Usuario;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    @Value("${security.jwt.expiracao}")
+    private String expiracao;
+
+    @Value("${security.jwt.chave-assinatura}")
+    private String chaveAssinatura;
+
+    public String gerarToken( Usuario usuario ){
+
+        long expString = Long.valueOf(expiracao);
+        LocalDateTime dataHoraExpiracao = LocalDateTime.now().plusMinutes(expString);
+        Instant instant = dataHoraExpiracao.atZone(ZoneId.systemDefault()).toInstant();
+        Date data = Date .from(instant);
+
+        return Jwts
+                .builder()
+                .setSubject(usuario.getLogin())
+                .setExpiration(data)
+                .signWith( SignatureAlgorithm.HS512, chaveAssinatura )
+                .compact();
+    }
+    public static void main(String[] args){
+        ConfigurableApplicationContext contexto = SpringApplication.run(VendasApplication.class);
+        JwtService service = contexto.getBean(JwtService.class);
+        Usuario usuario = Usuario.builder().login("fulano").build();
+        String token = service.gerarToken(usuario);
+        System.out.println(token);
+    }
+
+}
